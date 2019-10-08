52c1f09ae9a38b6bb9a402e878588c4d97d7396a
wendeng <w526529650@outlook.com>
2019-10-08 13:26:53 +0800

    实现分类页功能

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,14 +1,17 @@
 
 let url = {
     hotLists: '/index/hotLists',
-    banner: '/index/banner'
+    banner: '/index/banner',
+    topList: '/category/topList',
+    subList: '/category/subList',
+    rank: '/category/rank',
 }
 
 //开发环境和真实环境的切换
 // let host = 'http://rapapi.org/mockjsdata/24170'
-//  let host = 'http://rap2api.taobao.org/app/mock/7058'
+ let host = 'http://rap2api.taobao.org/app/mock/7058'
 //  let host = 'http://rapapi.org/mockjsdata/23334'
- let host = 'https://www.easy-mock.com/mock/5c9c3045d172204b3a07ecb0/youzan'
+//  let host = 'https://www.easy-mock.com/mock/5c9c3045d172204b3a07ecb0/youzan'
 for (let key in url) {
     if (url.hasOwnProperty(key)) {
         url[key] = host + url[key]
---
+++ src/pages/category/category.js
@@ -0,0 +1,50 @@
+import 'css/common.css'
+import './category.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+
+import Foot from 'components/Foot.vue'
+
+new Vue({
+    el:'#app',
+    data:{
+        topLists: null,
+        topIndex: 0 ,   //焦点状态的切换就是每一次点击以后要把当前的Index作为一个全局的赋值，然后拿到topIndex和当前的这个去比
+        subData : null,
+        rankData : null
+    },
+    created () {
+        this.getTopList()
+        this.getSubList(0)
+    },
+    methods: {
+      getTopList(){
+          axios.get(url.topList).then(res => {
+              this.topLists = res.data.lists
+          }).catch(res => {
+
+          })
+      },
+      getSubList(index,id){
+          console.log(index,id)
+        this.topIndex = index
+        if (index === 0) {
+            this.getRank()
+        } else {
+            axios.get(url.subList, { id }).then(res => {
+                this.subData = res.data.data
+            })
+        } 
+      },
+      getRank()  {
+        axios.get(url.rank).then(res => {
+            this.rankData = res.data.data
+        })
+      }
+    },
+    components: {
+        Foot
+    }
+})
