8c2a0529bf87f1dfb5e56a004271776d124427c1
wendeng <w526529650@outlook.com>
2019-10-11 22:57:09 +0800

    完成详情页

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -5,6 +5,7 @@ import './goods_mars.css'
 import './goods_sku.css'
 import './goods_theme.css'
 import './goods.css'
+import './goods_transition.css'
 
 import Vue from 'vue'
 import url from 'js/api.js'
@@ -19,11 +20,17 @@ let detailTab = ['商品详情','本店成交']
 new Vue({
     el:'#app',
     data : {
+        id,
         details : null,  
         detailTab, 
         tabIndex: 0,
         dealLists: null,
-        bannerLists: null
+        bannerLists: null,
+        skuType: 1,
+        showSku: false,
+        skuNum: 1,
+        isAddCart: false,
+        showAndMessage: false,
     },
     created () {
         this.getDetails()
@@ -33,7 +40,7 @@ new Vue({
             axios.get(url.details,{id}).then(res => {
                 this.details = res.data.data
                 this.bannerLists = []
-                this.details.imgs.forEach(item => {
+                this.details.imgs.forEach(item => {   //数据改造
                     this.bannerLists.push({
                         clickUrl:'',
                         img: item
@@ -53,9 +60,41 @@ new Vue({
                 this.getDeal()
             }
         },
+        chooseSku(type){
+            this.skuType = type
+            this.showSku = true
+        },
+        changeSkuNum(num){
+            if(num<0 && this.skuNum === 1) return
+            this.skuNum += num
+        },
+        addCart(){
+            axios.post(url.addCart,{
+                id,
+                number: this.skuNum
+            }).then(res => {
+                if(res.data.status === 200){
+                    this.showSku = false
+                    this.isAddCart = true
+                    this.showAndMessage = true
+
+                    setTimeout(()=>{
+                        this.showAndMessage = false
+                    },1000)
+                }
+            })
+        }
     },
     components: {
         Swipe
+    },
+    watch: {
+        showSku(val,oldVal){  //监听showSku，当弹出层弹出以后固定body以及html不能滚动
+            document.body.style.overflow = val ? 'hidden' : "auto"
+            document.querySelector('html').style.overflow = val ? 'hidden' : 'auto'
+            document.body.style.height = val ? '100%' : 'auto'
+            document.querySelector('html').style.height = val ? '100%' : 'auto'
+        }
     },
     mixins: [mixin]
 })
